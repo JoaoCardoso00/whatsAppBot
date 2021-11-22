a2b6cbc79a4fd0d95694b1c6f3526f2a9a53ce13
darkeaee <jvcq13@gmail.com>
2021-11-22 15:06:54 -0300

    teste1

--- src/routes/response.js
+++ src/routes/response.js
@@ -4,16 +4,16 @@ const router = Router();
 
 router.post('/', async (req, res) => {
   const data = req.body
-
+  
   try {
     await client.messages.create({ 
       body: `${data}`, 
       from: `whatsapp:+${process.env.PHONE_BOT}`,
       to: `whatsapp:+${process.env.PHONE_USER}` 
     }).done();
-    return res.send({ message: 'Send' });
+    return res.send({ message: `Send ${data}` });
   } catch (e) {
-    return res.status(400).send({ Error: e });
+    return res.status(400).send({ Error: [e, data] });
   }
 });
 
