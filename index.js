655c3ba1c9aa24eab3ddeccee3e13345fd9cb0c8
darkeaee <jvcq13@gmail.com>
2021-11-22 14:30:08 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import twilio from "twilio";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const { SID, AUTH_TOKEN } = process.env;
+const client = twilio(SID, AUTH_TOKEN);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', async (req, res) => {
+  try {
+    client.messages.create({ 
+      body: 'Your Yummy Cupcakes Company order of 1 dozen frosted cupcakes has shipped and should be delivered on July 10, 2019. Details: http://www.yummycupcakes.com/', 
+      from: `whatsapp:+${process.env.PHONE_BOT}`,
+      to: `whatsapp:+${process.env.PHONE_USER}` 
+    }).done();
+    return res.send({ message: 'Send' });
+  } catch (e) {
+    return res.send({ Error: e });
+  }
+});
+
+app.listen(PORT, () => console.log(`REST API running in PORT: ${PORT}`));
+//+1 415 523 8886 with code join influence-bad.
+
