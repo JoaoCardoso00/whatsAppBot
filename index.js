b3f6a4f0368c41b884b11fe4ac9bc8bb739f970a
darkeaee <jvcq13@gmail.com>
2021-11-22 15:15:11 -0300

    'test3'

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api', routes)
 
+app.get('/', (req, res) => {
+  return res.send({ Hello: 'World!' });
+});
+
 app.listen(PORT, () => console.log(`REST API running in PORT: ${PORT}`));
 //+1 415 523 8886 with code join influence-bad.
 
